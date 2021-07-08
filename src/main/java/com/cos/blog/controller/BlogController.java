434f0ccdeb6fbf8f9f42afb413db9ce78fb8f025
yd <aa@aa.com>
2021-07-09 01:14:29 +0900

    환경세팅 완료

---
+++ src/main/java/com/cos/blog/controller/BlogController.java
@@ -0,0 +1,14 @@
+package com.cos.blog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogController {
+	
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "<h1>hello sprng boot</h1>";
+	}
+
+}
